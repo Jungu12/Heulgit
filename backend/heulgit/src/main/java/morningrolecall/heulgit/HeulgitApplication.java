886c5286c9ad9d3744aa017683809095d57c3a6b
kjg_0701 <kjg_0701@naver.com>
2023-07-21 16:44:39 +0900

    init(all): 백엔드 프로젝트 세팅
    프로젝트 시작을 위한 초기 세팅
    - 스프링 부트 애플리케이션 생성
    - 의존성 추가

---
+++ backend/heulgit/src/main/java/morningrolecall/heulgit/HeulgitApplication.java
@@ -0,0 +1,13 @@
+package morningrolecall.heulgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeulgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeulgitApplication.class, args);
+	}
+
+}
