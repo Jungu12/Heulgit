3b3925c58fa2361a1ca7fb32bf2bff43e478d0d9
ksg2388 <ksg2388@naver.com>
2023-07-24 13:42:27 +0900

    init(all): 기본 폰트 설정
    - noto-sans kr로 기본 폰트 변경

--- frontend/src/global-style.ts
+++ frontend/src/global-style.ts
@@ -2,6 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 // 외부에서 import 할거니까 모듈 내보내자~!
 export default createGlobalStyle`
+
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
@@ -20,6 +21,7 @@ time, mark, audio, video {
 	border: 0;
 	font-size: 100%;
 	font: inherit;
+	font-family: 'Noto Sans KR', sans-serif;
 	vertical-align: baseline;
 }
 /* HTML5 display-role reset for older browsers */
