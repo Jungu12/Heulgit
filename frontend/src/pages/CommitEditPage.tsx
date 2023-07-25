90cd54f54f04e2429cd97a5ac7b9bff6224ada27
백승은 <tmdms12@gmail.com>
2023-07-25 12:28:02 +0900

    feat(profile): 프로필 페이지 퍼블리싱 (#4/#S09P12D211-405)
    프로필 페이지 1단계(모바일) 퍼블리싱
    - 마이페이지 구현
    - 커밋수정페이지 구현

--- frontend/src/pages/CommitEditPage.tsx
+++ frontend/src/pages/CommitEditPage.tsx
@@ -1,7 +1,83 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Header = styled.div`
+	height: 55px;
+`;
+const BackButton = styled.button``;
+
+const CommitPageMiddle = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+const CommitConvention = styled.div`
+	display: flex;
+	justify-content: space-between;
+	border: 1px solid;
+	border-radius: 5px;
+	width: 410px;
+	height: 55px;
+	padding: 5px;
+`;
+const CommitDescription = styled.div`
+	flex-direction: column;
+`;
+const CommitTag = styled.div`
+	border: 1px solid;
+	border-radius: 10px;
+	height: 21px;
+	width: 50px;
+`;
+
+const CommitPageLow = styled.div`
+	display: flex;
+	justify-content: center;
+	position: fixed;
+	bottom: 0;
+	width: 100%;
+	padding: 10px;
+`;
+
+const CommitPlusBox = styled.button`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border: 1px solid;
+	border-radius: 5px;
+	width: 410px;
+`;
+const Delete = styled.button``;
 
 const CommitEditPage = () => {
-	return <div>CommitEditPage</div>;
+	return (
+		<div>
+			<Header>
+				<BackButton>◀</BackButton>
+				커밋 메시지 설정
+			</Header>
+			<hr />
+			<CommitPageMiddle>
+				<CommitConvention>
+					<CommitDescription>
+						<CommitTag>#feat </CommitTag>
+						<div>새로운 기능 개발</div>
+					</CommitDescription>
+					<Delete>삭제</Delete>
+				</CommitConvention>
+				<CommitConvention>
+					<CommitDescription>
+						<CommitTag>#feat </CommitTag>
+						<div>새로운 기능 개발</div>
+					</CommitDescription>
+					<Delete>삭제</Delete>
+				</CommitConvention>
+			</CommitPageMiddle>
+			<CommitPageLow>
+				<CommitPlusBox>커밋 메시지 추가</CommitPlusBox>
+			</CommitPageLow>
+		</div>
+	);
 };
 
 export default CommitEditPage;
--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,7 +1,104 @@
+import Navigation from '@components/common/Navigation';
 import React from 'react';
+import styled from 'styled-components';
+
+const ProfileHigh = styled.div`
+	display: flex;
+	justify-content: space-around;
+`;
+
+const UserProfile = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 150px;
+`;
+
+const UserImage = styled.img`
+	display: flex;
+	justify-content: flex-start;
+	background-color: black;
+	height: 100px;
+	width: 100px;
+	border-radius: 50px;
+`;
+
+const UserInformation = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+`;
+
+const ProfileCategory = styled.div`
+	display: flex;
+	justify-content: space-around;
+	align-items: flex-end;
+	height: 50px;
+`;
+
+const ProfileLow = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
+
+const CommitBox = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border: 1px solid;
+	border-radius: 10px;
+	width: 410px;
+	height: 260px;
+	margin: 10px;
+`;
+
+const CommitRank = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border: 1px solid;
+	border-radius: 10px;
+	width: 410px;
+	height: 350px;
+	margin: 10px;
+`;
+
+const Footer = styled.div`
+	height: 50px;
+`;
 
 const ProfilePage = () => {
-	return <div>ProfilePage</div>;
+	return (
+		<div>
+			<ProfileHigh>
+				<UserProfile>
+					<UserImage src="" alt="User" />
+					<UserInformation>
+						<div>유저 이름</div>
+						<div>팔로잉 팔로워</div>
+						<div>추가 정보</div>
+					</UserInformation>
+				</UserProfile>
+				<div>내활동</div>
+			</ProfileHigh>
+			<ProfileCategory>
+				{/* 프로필 카테고리 */}
+				<div>프로필</div>
+				<div>유레카</div>
+				<div>자유</div>
+			</ProfileCategory>
+			<hr />
+			<ProfileLow>
+				<CommitBox>커밋 분석 그래프</CommitBox>
+				<CommitRank>커밋 랭킹</CommitRank>
+			</ProfileLow>
+			<Footer>
+				<Navigation />
+			</Footer>
+		</div>
+	);
 };
 
 export default ProfilePage;
