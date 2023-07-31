db81ed9d751f3255df712be0394124187d2ace2b
ksg2388 <ksg2388@naver.com>
2023-07-31 15:59:12 +0900

    fix(search): 100vh 적용 안되서 수정

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -9,7 +9,7 @@ import { styled } from 'styled-components';
 const StyledSearchContainer = styled.div`
 	display: flex;
 	flex-direction: column;
-	min-height: 100vh;
+	height: 100vh;
 
 	div {
 		margin-right: calc(50% - 62px);
