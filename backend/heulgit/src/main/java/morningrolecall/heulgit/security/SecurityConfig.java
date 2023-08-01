0d779c39343cca0f18b0fb56b5aea92c7a4a20a6
seongukbaek <bsu6235@gmail.com>
2023-08-01 14:16:53 +0900

    chore(config): CORS 적용 (#20)

--- backend/heulgit/src/main/java/morningrolecall/heulgit/security/SecurityConfig.java
+++ backend/heulgit/src/main/java/morningrolecall/heulgit/security/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
 		CorsConfiguration config = new CorsConfiguration();
 
 		config.setAllowCredentials(true);
-		config.setAllowedOrigins(List.of("*"));
+		config.setAllowedOrigins(List.of("http://localhost:3000", "https://i9d211.p.ssafy.io"));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
 		config.setAllowedHeaders(List.of("*"));
 		config.setExposedHeaders(List.of("*"));
