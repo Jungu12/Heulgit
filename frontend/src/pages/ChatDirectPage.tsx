86a4d624152d3483b5dad201e1e6fe3338b5d2d6
ksg2388 <ksg2388@naver.com>
2023-08-02 16:15:29 +0900

    feat(gm): 채팅 기능 구현(#23)
    채팅 기능 구현
    - sockJS, stomp 라이브러리 추가
    - 서버와 통신하여 채팅 기능 구현

--- frontend/src/pages/ChatDirectPage.tsx
+++ frontend/src/pages/ChatDirectPage.tsx
@@ -1,29 +1,40 @@
-import React, { useState, KeyboardEvent, useRef, useEffect } from 'react';
+import React, {
+	useState,
+	KeyboardEvent,
+	useRef,
+	useEffect,
+	useCallback,
+} from 'react';
 import Header from '@components/common/Header';
 import ChatBox from '@components/gm/ChatBox';
 import { colors } from '@constants/colors';
 import { styled } from 'styled-components';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useLocation } from 'react-router';
+import { ChatRoomType, MessageType } from '@typedef/gm/gm.types';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { CompatClient, Stomp } from '@stomp/stompjs';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import SockJS from 'sockjs-client';
+import { http } from '@utils/http';
 
-type Message = {
-	text: string;
-	$isUser: boolean;
-};
-
-type ChatDirectPageState = {
-	inputMessage: string;
-	messages: Message[];
-};
+const StyledChatDirectPageContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	height: calc(var(--vh, 1vh) * 100);
+`;
 
 const StyledHeader = styled.div`
 	width: 100%;
 	height: 56px;
-	margin-bottom: 10px;
 `;
 
 const StyledMessage = styled.div`
 	word-wrap: break-word;
 	word-break: break-all;
 	margin-bottom: 50px;
+	height: calc((var(--vh, 1vh) * 100) - 102px);
+	overflow: scroll;
 `;
 
 const StyledFooter = styled.div`
@@ -53,7 +64,6 @@ const StyledInputDiv = styled.div<StyledInputDivProps>`
 	border-radius: 8px;
 	padding: 2px 6px;
 	height: 35px;
-	/* margin: 10px 0; */
 `;
 
 const StyledInput = styled.input`
@@ -71,79 +81,141 @@ const StyledButton = styled.button`
 	border-radius: 8px;
 `;
 
-const ChatDirectPage: React.FC = () => {
-	const [state, setState] = useState<ChatDirectPageState>({
-		inputMessage: '',
-		messages: [
-			{ text: '야야 뭐하고있어', $isUser: false },
-			{ text: '코딩지옥...', $isUser: true },
-			{ text: '진짜 집가고싶다ㅠㅠ', $isUser: false },
-			{ text: '침대가 나를 기다린다...', $isUser: false },
-			{
-				text: '두줄이상은 어떻게 되는지 확인해야지... 길게길게 써봅시다... 리액트 너무너무 재밌다...즐겁다...',
-				$isUser: true,
-			},
-		],
-	});
+type RouteState = {
+	state: {
+		room: ChatRoomType;
+	};
+};
+
+const ChatDirectPage = () => {
+	const { state } = useLocation() as RouteState;
+	const client = useRef<CompatClient>();
+	const messageEndRef = useRef<HTMLDivElement | null>(null);
+	const [messageList, setMessageList] = useState<MessageType[]>([]);
+	const [message, setMessage] = useState<MessageType>();
+	const [inputMessage, setInputMessage] = useState('');
+
+	// 채팅방 연결 함수
+	const connectHandler = useCallback((roomId: string) => {
+		client.current = Stomp.over(() => {
+			const sock = new SockJS('http://192.168.100.64:8080/gm');
+			return sock;
+		});
+
+		setMessageList([...state.room.chatMessages]);
+		client.current.connect({}, () => {
+			client.current!.subscribe(
+				`/sub/chat/room/${roomId}`,
+				(msg) => {
+					setMessage(JSON.parse(msg.body));
+				},
+				{ Authorization: '', simpDestination: roomId },
+			);
+		});
+	}, []);
+
+	// 메세지 보내기 함수
+	const sendHandler = useCallback(() => {
+		if (inputMessage.trim() === '') return;
+		console.log(inputMessage);
+
+		client.current?.send(
+			'/pub/chat/message',
+			{},
+			JSON.stringify({
+				roomId: state.room.roomId,
+				sender: 'ksg2388',
+				message: inputMessage,
+			}),
+		);
+		setInputMessage('');
+	}, [inputMessage]);
 
 	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setState((prev) => ({ ...prev, inputMessage: e.target.value }));
-	};
-
-	const handleSubmit = () => {
-		const { inputMessage, messages } = state;
-		if (inputMessage.trim() === '') {
-			return;
-		}
-		setState({
-			inputMessage: '',
-			messages: [...messages, { text: inputMessage, $isUser: true }],
-		});
+		setInputMessage(e.target.value);
 	};
 
 	const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
 		if (e.key === 'Enter') {
-			handleSubmit();
+			sendHandler();
 		}
 	};
-	const messageEndRef = useRef<HTMLDivElement | null>(null);
+
+	const loadChatRoomLog = useCallback((id: string) => {
+		http
+			.get<MessageType[]>(`http://192.168.100.64:8080/gm/chats/${id}`)
+			.then((res) => {
+				// console.log(res);
+				setMessageList([...res]);
+				// setChatRoomList(res);
+			});
+	}, []);
 
 	useEffect(() => {
+		// console.log('[메세지 리스트]', messageList);
+
 		if (messageEndRef.current) {
-			messageEndRef.current.scrollIntoView({ behavior: 'smooth' });
+			messageEndRef.current.scrollIntoView({
+				behavior: 'auto',
+				block: 'end',
+				inline: 'nearest',
+			});
 		}
-	}, [state.messages]);
+	}, [messageList]);
+
+	// useEffect(() => {
+	// 	console.log(inputMessage);
+	// }, [inputMessage]);
+
+	// useEffect(() => {
+	// 	console.log('[message]', state.room.chatMessages);
+	// }, [state]);
+
+	useEffect(() => {
+		connectHandler(state.room.roomId);
+	}, []);
+
+	useEffect(() => {
+		if (message) {
+			setMessageList([...messageList, message]);
+		}
+		// console.log(message);
+	}, [message]);
+
+	useEffect(() => {
+		loadChatRoomLog(state.room.roomId);
+	}, [state.room.roomId]);
 
 	return (
-		<div>
+		<StyledChatDirectPageContainer>
 			<StyledHeader>
 				<Header title={'userName'} />
 			</StyledHeader>
 			<StyledMessage>
-				{state.messages.map((message, index) => (
+				{messageList.map((msg, index) => (
 					<ChatBox
 						key={index}
-						message={message.text}
-						$isUser={message.$isUser}
+						message={msg.message}
+						$isUser={msg.sender === 'ksg2388'}
 					/>
 				))}
 				<div ref={messageEndRef}></div>
 			</StyledMessage>
 			<StyledFooter>
 				<StyledInputWrap>
-					<StyledInputDiv $isEmpty={state.inputMessage === ''}>
+					<StyledInputDiv $isEmpty={inputMessage === ''}>
 						<StyledInput
 							type="text"
-							value={state.inputMessage}
+							value={inputMessage}
 							onChange={handleInputChange}
-							onKeyPress={handleKeyPress}
+							onKeyDown={handleKeyPress}
 						/>
 						<button>사진</button>
 					</StyledInputDiv>
 				</StyledInputWrap>
-				<StyledButton onClick={handleSubmit}>전송</StyledButton>
+				<StyledButton onClick={sendHandler}>전송</StyledButton>
 			</StyledFooter>
-		</div>
+		</StyledChatDirectPageContainer>
 	);
 };
 
