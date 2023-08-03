bb46d12a68187bb75d29c780d2152b6031d34325
kjg_0701 <kjg_0701@naver.com>
2023-08-03 22:43:28 +0900

    feat(relation): 팔로우 API 구현(#22/#S09P12D211-748/#S09P12D211-749/#S09P12D211-751/#S09P12D211-752/#S09P12D211-754)
    팔로우 API 구현
    - 팔로우 상태를 확인하는 API 구현
    - 팔로우를 취소하는 API 구현
    - 팔로우 추가하는 API 구현
    - 팔로워 목록을 확인하는 API 구현
    - 팔로워 목록을 확인하는 API 구현
    - 팔로잉 목록을 확인하는 API 구현

--- backend/heulgit/src/main/java/morningrolecall/heulgit/relation/controlller/RelationController.java
+++ backend/heulgit/src/main/java/morningrolecall/heulgit/relation/controlller/RelationController.java
@@ -3,7 +3,10 @@ package morningrolecall.heulgit.relation.controlller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,49 +30,59 @@ public class RelationController {
 	 * @param to : follow 여부를 확인하고 싶은 user
 	 */
 	@GetMapping("/state")
-	public ResponseEntity<?> checkFollowState(@RequestParam String from, @RequestParam String to) {
-		logger.debug("followStateCheck(), ");
+	public ResponseEntity<?> checkFollowState(@AuthenticationPrincipal String from, @RequestParam String to) {
+
+		logger.debug("followStateCheck(), from = {}, to = {}", from, to);
+
 		return ResponseEntity.ok().body(relationService.checkFollowState(from, to));
 	}
 
-	// /**
-	//  * 사용자는 자신이 팔로우하는 다른 유저와 팔로우 관계를 끊을 수 있다.
-	//  * @param from : 사용자
-	//  * @param to : 사용자가 팔로우를 취소하려는 다른 유저 Github ID
-	//  */
-	// @PutMapping("/unfollow")
-	// public ResponseEntity<?> cancelFollow(@RequestParam String from, @RequestParam String to) {
-	//
-	// 	relationService.
-	//
-	// 	return ResponseEntity.ok().build();
-	// }
-	//
-	// /**
-	//  * 사용자는 다른 유저를 팔로우 할 수 있다.
-	//  * @param from : 사용자
-	//  * @param userId : 사용자가 팔로우하려는 다른 유저 Github ID
-	//  */
-	// @PostMapping("/follow")
-	// public ResponseEntity<?> addFollow(@AuthenticationPrincipal String to, @PathVariable String userId) {
-	// 	return ResponseEntity.ok().body();
-	// }
-	//
-	// /**
-	//  * 사용자는 자신을 팔로우하는 다른 유저들을 확인할 수 있다.
-	//  * @param userId : 사용자가 팔로우하려는 다른 유저 Github ID
-	//  */
-	// @GetMapping("/followers/{userId}")
-	// public ResponseEntity<?> getFollowers(@AuthenticationPrincipal String to, @PathVariable String userId) {
-	// 	return ResponseEntity.ok().build();
-	// }
-	//
-	// /**
-	//  * 사용자는 자신이 팔로우하는 다른 유저들을 확인할 수 있다.
-	//  * @param userId : 사용자가 팔로앙 중인 다른 유저 Github ID
-	//  */
-	// @GetMapping("/followings/{userId}")
-	// public ResponseEntity<?> getFollowings(@AuthenticationPrincipal String to, @PathVariable String userId) {
-	//
-	// }
+	/**자신이 팔로우하는 다른 유저와 팔로우 관계를 끊을 수 있다.
+	 * @param from : 사용자
+	 * @param to : 사용자가 팔로우를 취소하려는 다른 유저 Github ID
+	 */
+	@DeleteMapping("/unfollow")
+	public ResponseEntity<?> cancelFollow(@AuthenticationPrincipal String from, @RequestParam String to) {
+
+		logger.debug("cancelFollow(), from = {}, to = {}", from, to);
+		relationService.cancelFollow(from, to);
+
+		return ResponseEntity.ok().build();
+	}
+
+	/**
+	 * 사용자는 다른 유저를 팔로우 할 수 있다.
+	 * @param from : 사용자
+	 * @param userId : 사용자가 팔로우하려는 다른 유저 Github ID
+	 */
+	@PostMapping("/follow")
+	public ResponseEntity<?> addFollow(@AuthenticationPrincipal String from, @RequestParam String to) {
+
+		logger.debug("addFollow(), from = {}, to = {}", from, to);
+		relationService.addFollow(from, to);
+
+		return ResponseEntity.ok().build();
+	}
+
+	/**
+	 * 사용자는 자신을 팔로우하는 다른 유저들을 확인할 수 있다.
+	 * @param userId : 사용자가 팔로우하려는 다른 유저 Github ID
+	 */
+	@GetMapping("/followers")
+	public ResponseEntity<?> getFollowers(@AuthenticationPrincipal String userId) {
+
+		logger.debug("getFollowers(), userId = {}");
+		return ResponseEntity.ok().body(relationService.getFollowers(userId));
+	}
+
+	/**
+	 * 사용자는 자신이 팔로우하는 다른 유저들을 확인할 수 있다.
+	 * @param userId : 사용자가 팔로앙 중인 다른 유저 Github ID
+	 */
+	@GetMapping("/followings")
+	public ResponseEntity<?> getFollowings(@AuthenticationPrincipal String userId) {
+
+		logger.debug("getFollowings(), userId = {}");
+		return ResponseEntity.ok().body(relationService.getFollowings(userId));
+	}
 }
--- backend/heulgit/src/main/java/morningrolecall/heulgit/relation/service/RelationService.java
+++ backend/heulgit/src/main/java/morningrolecall/heulgit/relation/service/RelationService.java
@@ -1,8 +1,14 @@
 package morningrolecall.heulgit.relation.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import morningrolecall.heulgit.relation.domain.Relation;
 import morningrolecall.heulgit.relation.repository.RelationRepository;
 
 @Service
@@ -14,4 +20,40 @@ public class RelationService {
 	public boolean checkFollowState(String from, String to) {
 		return relationRepository.existsByFromIdAndToId(from, to);
 	}
+
+	@Transactional
+	public void cancelFollow(String from, String to) {
+		int deleteCount = relationRepository.deleteByFromIdAndToId(from, to);
+
+		if (deleteCount == 0) {
+			throw new RuntimeException("삭제할 정보가 없습니다.");
+		}
+	}
+
+	@Transactional
+	public void addFollow(String from, String to) {
+		// 이미 팔로우 관계가 존재하면 아무 작업을 하지 않는다.
+		if (relationRepository.existsByFromIdAndToId(from, to)) {
+			return;
+		}
+
+		// 팔로우 관계가 존재하지 않는 경우 새로운 Relation 엔티티를 생성하여 저장한다.
+		Relation relation = new Relation().builder()
+			.fromId(from)
+			.toId(to)
+			.build();
+
+		relationRepository.save(relation);
+	}
+
+	public List<String> getFollowers(String userId) {
+		List<Relation> relations = relationRepository.findByToId(userId);
+		return relations.stream().map(Relation::getFromId).collect(Collectors.toList());
+	}
+
+	public List<String> getFollowings(String userId) {
+		List<Relation> relations = relationRepository.findByFromId(userId);
+		return relations.stream().map(Relation::getToId).collect(Collectors.toList());
+	}
 }
+
