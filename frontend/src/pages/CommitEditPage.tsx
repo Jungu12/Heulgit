2c83cf1caf3ccc17f576c85e41c90f0189bbf35f
백승은 <tmdms12@gmail.com>
2023-08-07 16:49:29 +0900

    feat(mypage): 커밋 편집 페이지 모달 추가 (#4/#S09P12D211-942)
    - 커밋 추가 모달창 생성

--- frontend/src/pages/CommitEditPage.tsx
+++ frontend/src/pages/CommitEditPage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import ReactModal from 'react-modal';
 import Header from '@components/common/Header';
 import BigHeader from '@components/profile/BigHeader';
 import CommitTag from '@components/profile/Commit';
@@ -27,13 +28,11 @@ const StyledSideL = styled.div`
 	@media (max-width: 767px) {
 		display: none;
 	}
-
 	@media (min-width: 768px) {
 		position: fixed;
 		width: 124px;
 		background-color: ${colors.primary.primaryLighten};
 	}
-
 	@media (min-width: 1200px) {
 		position: fixed;
 		width: 242px;
@@ -47,13 +46,11 @@ const StyledSideR = styled.div`
 	@media (max-width: 767px) {
 		display: none;
 	}
-
 	@media (min-width: 768px) {
 		position: fixed;
 		width: 124px;
 		background-color: ${colors.primary.primaryLighten};
 	}
-
 	@media (min-width: 1200px) {
 		position: fixed;
 		width: 242px;
@@ -102,7 +99,7 @@ const StyledFooter = styled.div`
 `;
 const CommitPlusButton = styled.button`
 	width: 100%;
-	height: 35px;
+	height: 40px;
 
 	margin: 10px;
 	padding: 10px;
@@ -117,6 +114,86 @@ const CommitPlusButton = styled.button`
 
 	background-color: ${colors.primary.primary};
 	color: white;
+`;
+
+// 모달
+const customStyles = {
+	overlay: {
+		zIndex: '99',
+		backgroundColor: 'rgba(0, 0, 0, 0.3)', // 오버레이 배경색을 투명하게 설정
+	},
+	content: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		padding: '20px',
+		margin: 'auto',
+		maxWidth: '500px',
+		height: '400px',
+		background: 'rgba(255, 255, 255)',
+		backdropFilter: 'blur(10px)',
+		// webkitBackdropFilter: 'blur(10px)', // -webkit-backdrop-filter 추가
+		zIndex: '99',
+		border: 'none',
+		borderRadius: '20px',
+	},
+};
+const StyledModalContent = styled.div<{
+	$inputContent: boolean;
+	$textareaContent: boolean;
+}>`
+	width: 100%;
+	font-size: 20px;
+	div {
+	}
+	label {
+		margin: 30px 0 10px 0;
+	}
+	input {
+		border: 2px solid;
+		border-radius: 10px;
+		padding: 10px;
+		border-color: ${(props) =>
+			props.$inputContent ? colors.primary.primary : colors.greyScale.grey3};
+	}
+	textarea {
+		height: 100px;
+		border: 2px solid;
+		border-radius: 10px;
+		padding: 10px;
+		border-color: ${(props) =>
+			props.$textareaContent ? colors.primary.primary : colors.greyScale.grey3};
+		resize: none;
+	}
+	input:focus,
+	textarea:focus {
+		outline: 1px solid ${colors.primary.primary};
+		border: 2px solid ${colors.primary.primary};
+	}
+`;
+const StyledModalItem = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+const StyledModalButtonItem = styled.div`
+	display: flex;
+	justify-content: end;
+	button {
+		border-radius: 10px;
+		margin: 10px;
+		padding: 10px;
+	}
+	button.modal-close {
+		background-color: ${colors.point.red};
+		color: white;
+	}
+	button.modal-submit {
+		color: white;
+		background-color: ${colors.primary.primary};
+	}
+	button.modal-submit:disabled {
+		background-color: ${colors.primary.primaryLighten};
+	}
 `;
 
 const CommitEditPage = () => {
@@ -164,6 +241,30 @@ const CommitEditPage = () => {
 		};
 	}, []);
 
+	// 모달
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [newCommit, setNewCommit] = useState({ title: '', detail: '' });
+	const handleOpenModal = () => {
+		setIsModalOpen(true);
+	};
+	const handleCloseModal = () => {
+		setNewCommit({ title: '', detail: '' });
+		setIsModalOpen(false);
+	};
+	const handleAddCommit = () => {
+		if (newCommit.title && newCommit.detail) {
+			setCommitTags((prevTags) => [
+				...prevTags,
+				{ ...newCommit, id: Date.now() },
+			]);
+			setNewCommit({ title: '', detail: '' });
+			setIsModalOpen(false);
+		}
+	};
+	const $inputContent = Boolean(newCommit.title);
+	const $textareaContent = Boolean(newCommit.detail);
+	const isSubmitDisabled = !newCommit.title || !newCommit.detail;
+
 	return (
 		<StyledBox>
 			<StyledSideL>
@@ -195,12 +296,63 @@ const CommitEditPage = () => {
 					))}
 				</CommitPageMiddle>
 				<StyledFooter>
-					<CommitPlusButton>커밋 메시지 추가</CommitPlusButton>
+					<CommitPlusButton onClick={handleOpenModal}>
+						커밋 메시지 추가
+					</CommitPlusButton>
 				</StyledFooter>
 			</StyledCommitEditPage>
 			<StyledSideR>
 				<div>카테고리</div>
 			</StyledSideR>
+
+			{/* 모달 */}
+			<ReactModal
+				isOpen={isModalOpen}
+				style={customStyles}
+				onRequestClose={handleCloseModal}
+				contentLabel="커밋 메시지 추가"
+			>
+				<StyledModalContent
+					$inputContent={$inputContent}
+					$textareaContent={$textareaContent}
+				>
+					<StyledModalItem>
+						<label htmlFor="commitTitle">커밋 타입</label>
+						<input
+							maxLength={10}
+							type="text"
+							id="commitTitle"
+							value={newCommit.title}
+							onChange={(e) =>
+								setNewCommit({ ...newCommit, title: e.target.value })
+							}
+						/>
+					</StyledModalItem>
+					<StyledModalItem>
+						<label htmlFor="commitDetail">타입 설명</label>
+						<textarea
+							maxLength={40}
+							id="commitDetail"
+							value={newCommit.detail}
+							onChange={(e) =>
+								setNewCommit({ ...newCommit, detail: e.target.value })
+							}
+						/>
+					</StyledModalItem>
+					<StyledModalButtonItem>
+						<button onClick={handleCloseModal} className="modal-close">
+							취소
+						</button>
+						<button
+							onClick={handleAddCommit}
+							className="modal-submit"
+							disabled={isSubmitDisabled}
+						>
+							완료
+						</button>
+					</StyledModalButtonItem>
+				</StyledModalContent>
+			</ReactModal>
 		</StyledBox>
 	);
 };
