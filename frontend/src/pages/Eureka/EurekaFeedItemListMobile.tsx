77c477c9cf62d2fbdda5329be8aafa32b18858b2
ksg2388 <ksg2388@naver.com>
2023-08-09 15:44:59 +0900

    feat(eureka): 무한 리스트 구현
    - 피드 무한 리스트로 불러오기 구현

--- frontend/src/pages/Eureka/EurekaFeedItemListMobile.tsx
+++ frontend/src/pages/Eureka/EurekaFeedItemListMobile.tsx
@@ -1,13 +1,14 @@
 // 유레카 피드 리스트 모바일 버전
-
-import React from 'react';
+import React, { useRef } from 'react';
 import { styled } from 'styled-components';
 import { colors } from '@constants/colors';
 import { EurekaPostType } from '@typedef/community/eureka.types';
 import EurekaFeedItem from './EurekaFeedItem';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const StyledFeedListSection = styled.section`
 	overflow-y: scroll;
+	height: calc(100vh - 234px);
 	margin-bottom: 70px;
 	scrollbar-width: none; /* 파이어폭스 */
 	/* ( 크롬, 사파리, 오페라, 엣지 ) 동작 */
@@ -25,17 +26,33 @@ const Separation = styled.div`
 
 type Props = {
 	feedList: EurekaPostType[];
+	eurekaHasMore: boolean;
+	eurekaNextPageLoad: () => Promise<void>;
 };
 
-const EurekaFeedItemListMobile = ({ feedList }: Props) => {
+const EurekaFeedItemListMobile = ({
+	feedList,
+	eurekaHasMore,
+	eurekaNextPageLoad,
+}: Props) => {
+	const scrollContinaerRef = useRef<HTMLDivElement>(null);
+
 	return (
-		<StyledFeedListSection>
-			{feedList.map((feed, index) => (
-				<div key={index}>
-					<EurekaFeedItem feed={feed} />
-					<Separation />
-				</div>
-			))}
+		<StyledFeedListSection className="thisThis" ref={scrollContinaerRef}>
+			<InfiniteScroll
+				dataLength={feedList.length}
+				next={eurekaNextPageLoad}
+				hasMore={eurekaHasMore}
+				loader={<div>로딩중...</div>}
+				height={`calc(100vh - 234px)`}
+			>
+				{feedList.map((feed, index) => (
+					<div key={index}>
+						<EurekaFeedItem feed={feed} />
+						<Separation />
+					</div>
+				))}
+			</InfiniteScroll>
 		</StyledFeedListSection>
 	);
 };
