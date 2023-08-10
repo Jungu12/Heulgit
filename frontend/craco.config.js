7f38ff0960fea3ec1a86dfa1c5e6ac2742526eee
ksg2388 <ksg2388@naver.com>
2023-08-10 14:40:24 +0900

    fix(all): 인증서 경로 주석 처리

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const CracoAlias = require('craco-alias');
 
 module.exports = {
-	devServer: {
-		https: {
-			key: fs.readFileSync('C:/Users/SSAFY/Desktop/localhost-key.pem'),
-			cert: fs.readFileSync('C:/Users/SSAFY/Desktop/localhost.pem'),
-		},
-	},
+	// devServer: {
+	// 	https: {
+	// 		key: fs.readFileSync('C:/Users/SSAFY/Desktop/localhost-key.pem'),
+	// 		cert: fs.readFileSync('C:/Users/SSAFY/Desktop/localhost.pem'),
+	// 	},
+	// },
 	plugins: [
 		{
 			plugin: CracoAlias,
