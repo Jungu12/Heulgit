5a33c81fefadf71731cf35c7c476e5bd29ff738e
kjg_0701 <kjg_0701@naver.com>
2023-08-13 17:08:58 +0900

    feat(login): 사용자 login시 refresh token redis 저장소에 저장하는 로직 구현

---
+++ backend/heulgit/src/main/java/morningrolecall/heulgit/auth/interceptor/AuthInterceptor.java
@@ -0,0 +1,27 @@
+package morningrolecall.heulgit.auth.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import morningrolecall.heulgit.auth.util.JwtProvider;
+import morningrolecall.heulgit.auth.util.JwtRedisManager;
+
+@Component
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthInterceptor implements HandlerInterceptor {
+
+	private JwtRedisManager jwtRedisManager;
+	private JwtProvider jwtProvider;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws
+		Exception {
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+}
