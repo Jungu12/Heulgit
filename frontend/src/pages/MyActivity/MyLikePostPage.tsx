d9488ff72fb03c8614dde63763a4e846712187c9
ksg2388 <ksg2388@naver.com>
2023-08-13 22:47:58 +0900

    fix(freeboard): 안 쓰는 코드 주석 처리

--- frontend/src/pages/MyActivity/MyLikePostPage.tsx
+++ frontend/src/pages/MyActivity/MyLikePostPage.tsx
@@ -6,73 +6,73 @@ import Category from '@components/profile/Category';
 import CategoryT from '@components/profile/CategoryT';
 import { colors } from '@constants/colors';
 // import EurekaFeedItemListMobile from '@pages/Eureka/EurekaFeedItemListMobile';
-import FreeBoardFeedItemListMobile from '@pages/freeboard/FreeBoardFeedItemListMobile';
-import { images } from '@constants/images';
+// import FreeBoardFeedItemListMobile from '@pages/freeboard/FreeBoardFeedItemListMobile';
+// import { images } from '@constants/images';
 
 //더미데이터
-const dummyFreePosts = [
-	{
-		id: 1,
-		title: 'hihi',
-		user: {
-			id: 'jungu12',
-			avater_url: images.dummy.dummy1,
-		},
-		content: 'eqweqwewq',
-		link: 'dfkldfsdfds',
-		updated_date: '2023-07-24',
-		views: 100,
-		likes: 10,
-		comments: 12,
-		images: [{ file_uri: '' }],
-	},
-	{
-		id: 2,
-		title: '일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십',
-		user: {
-			id: 'jungu12',
-			avater_url: images.dummy.dummy1,
-		},
-		content:
-			'일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십',
-		link: 'dfkldfsdfds',
-		updated_date: '2023-07-24',
-		views: 100,
-		likes: 10,
-		comments: 12,
-		images: [{ file_uri: images.dummy.dummy3 }],
-	},
-	{
-		id: 4,
-		title: 'hihi',
-		user: {
-			id: 'jungu12',
-			avater_url: images.dummy.dummy1,
-		},
-		content: 'eqweqwewq',
-		link: 'dfkldfsdfds',
-		updated_date: '2023-07-24',
-		views: 100,
-		likes: 10,
-		comments: 12,
-		images: [{ file_uri: '' }],
-	},
-	{
-		id: 3,
-		title: 'hihi',
-		user: {
-			id: 'jungu12',
-			avater_url: images.dummy.dummy1,
-		},
-		content: 'asdasdasdasdasd',
-		link: 'dfkldfsdfds',
-		updated_date: '2023-07-24',
-		views: 100,
-		likes: 10,
-		comments: 12,
-		images: [{ file_uri: images.dummy.dummy1 }],
-	},
-];
+// const dummyFreePosts = [
+// 	{
+// 		id: 1,
+// 		title: 'hihi',
+// 		user: {
+// 			id: 'jungu12',
+// 			avater_url: images.dummy.dummy1,
+// 		},
+// 		content: 'eqweqwewq',
+// 		link: 'dfkldfsdfds',
+// 		updated_date: '2023-07-24',
+// 		views: 100,
+// 		likes: 10,
+// 		comments: 12,
+// 		images: [{ file_uri: '' }],
+// 	},
+// 	{
+// 		id: 2,
+// 		title: '일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십',
+// 		user: {
+// 			id: 'jungu12',
+// 			avater_url: images.dummy.dummy1,
+// 		},
+// 		content:
+// 			'일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십',
+// 		link: 'dfkldfsdfds',
+// 		updated_date: '2023-07-24',
+// 		views: 100,
+// 		likes: 10,
+// 		comments: 12,
+// 		images: [{ file_uri: images.dummy.dummy3 }],
+// 	},
+// 	{
+// 		id: 4,
+// 		title: 'hihi',
+// 		user: {
+// 			id: 'jungu12',
+// 			avater_url: images.dummy.dummy1,
+// 		},
+// 		content: 'eqweqwewq',
+// 		link: 'dfkldfsdfds',
+// 		updated_date: '2023-07-24',
+// 		views: 100,
+// 		likes: 10,
+// 		comments: 12,
+// 		images: [{ file_uri: '' }],
+// 	},
+// 	{
+// 		id: 3,
+// 		title: 'hihi',
+// 		user: {
+// 			id: 'jungu12',
+// 			avater_url: images.dummy.dummy1,
+// 		},
+// 		content: 'asdasdasdasdasd',
+// 		link: 'dfkldfsdfds',
+// 		updated_date: '2023-07-24',
+// 		views: 100,
+// 		likes: 10,
+// 		comments: 12,
+// 		images: [{ file_uri: images.dummy.dummy1 }],
+// 	},
+// ];
 // const dummyEurekaPosts = [
 // 	{
 // 		id: 1,
