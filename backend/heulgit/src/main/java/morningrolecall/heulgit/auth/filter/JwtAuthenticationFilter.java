3ea5f6618ab04c52edb6408b5466ca4fa6357905
kjg_0701 <kjg_0701@naver.com>
2023-08-14 17:31:11 +0900

    refactor(gm): filter 수정

--- backend/heulgit/src/main/java/morningrolecall/heulgit/auth/filter/JwtAuthenticationFilter.java
+++ backend/heulgit/src/main/java/morningrolecall/heulgit/auth/filter/JwtAuthenticationFilter.java
@@ -33,16 +33,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 		String requestURI = request.getRequestURI();
 
 		// oauth, swagger ui에 대한 URL은 필터 처리 X
-		// if (requestURI.startsWith("/api/oauth") || requestURI.startsWith("/v3/api-docs") || requestURI.startsWith(
-		// 	"/swagger-ui") || requestURI.startsWith("/favicon.ico") || requestURI.startsWith("/swagger-resources")) {
-		// 	filterChain.doFilter(request, response);
-		// 	return;
-		// }
-
-		if (requestURI.startsWith("/")) {
+		if (requestURI.startsWith("/api/oauth") || requestURI.startsWith("/v3/api-docs") || requestURI.startsWith(
+			"/swagger-ui") || requestURI.startsWith("/favicon.ico") || requestURI.startsWith("/swagger-resources")) {
 			filterChain.doFilter(request, response);
 			return;
 		}
+
+		// if (requestURI.startsWith("/")) {
+		// 	filterChain.doFilter(request, response);
+		// 	return;
+		// }
 
 		String token = jwtProvider.resolveToken(request);
 
