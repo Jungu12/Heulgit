def88c7079cb1be943d5e8c4932bf7f964b43305
ksg2388 <ksg2388@naver.com>
2023-08-14 23:06:20 +0900

    fix(gm): 소켓 연결 url 수정

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -33,7 +33,7 @@ const ChatPage = () => {
 	// 채팅방 연결 함수
 	const connectHandler = useCallback(() => {
 		client.current = Stomp.over(() => {
-			const sock = new SockJS('https://i9d211.p.ssafy.io/gm', {
+			const sock = new SockJS('wss://i9d211.p.ssafy.io/gm', {
 				headers: { Authorization: `Bearer ${accessToken}` },
 			});
 			return sock;
