2f3f3f467d3ea223dd5bfeaa0f7e3ef9f11811f3
kjg_0701 <kjg_0701@naver.com>
2023-08-16 22:41:05 +0900

    chore(config): swagger 설정 변경

--- backend/heulgit/src/main/java/morningrolecall/heulgit/config/WebConfig.java
+++ backend/heulgit/src/main/java/morningrolecall/heulgit/config/WebConfig.java
@@ -43,7 +43,7 @@ public class WebConfig implements WebMvcConfigurer {
 			.apis(RequestHandlerSelectors.basePackage("morningrolecall.heulgit"))
 			.paths(PathSelectors.any())
 			.build()
-			.host("http://i9d211.p.ssafy.io");
+			.host("https://i9d211.p.ssafy.io");
 	}
 
 	// @Override
