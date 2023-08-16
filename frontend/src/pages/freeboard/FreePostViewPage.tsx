fa7a8b28fb24786916a736e5a00638eb6d765f50
안솔비 <caca30@naver.com>
2023-08-17 00:30:31 +0900

    fix(freeboard): 유레카 유저 페이지 이동 작업 중

--- frontend/src/pages/freeboard/FreePostViewPage.tsx
+++ frontend/src/pages/freeboard/FreePostViewPage.tsx
@@ -198,10 +198,8 @@ const FreePostViewPage = () => {
 
 	// 유저 프로필 클릭시 유저 마이페이지로 이동
 	const onClickUserProfile = useCallback(() => {
-		if (feed?.user.githubId === userId) {
-			navigation(`/profiles/${feed?.user.githubId}`);
-		}
-	}, [feed, userId]);
+		navigation(`/profiles/${feed?.user.githubId}`);
+	}, []);
 
 	// 포스트 불러오기
 	const loadPost = useCallback(() => {
