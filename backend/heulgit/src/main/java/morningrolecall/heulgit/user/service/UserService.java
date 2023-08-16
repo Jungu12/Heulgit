f1cceb6db28e045ecec58686d4988390df826175
kjg_0701 <kjg_0701@naver.com>
2023-08-16 19:45:38 +0900

    refactor(user): 커밋 타입 파싱 로직 수정

--- backend/heulgit/src/main/java/morningrolecall/heulgit/user/service/UserService.java
+++ backend/heulgit/src/main/java/morningrolecall/heulgit/user/service/UserService.java
@@ -204,11 +204,10 @@ public class UserService {
 			int count = 0;
 
 			for (String commitMessage : commitMessages) {
-
-				String[] commit = commitMessage.split("\\(");
+				String[] commit = commitMessage.split(":");
 				String commitType = commit[0];
 
-				if (commitType.equals(type)) {
+				if (commitType.contains(type)) {
 					count++;
 				}
 			}
@@ -233,7 +232,7 @@ public class UserService {
 				commits.add(commitMessage);
 			}
 		} catch (Exception e) {
-			throw new AuthException(ExceptionCode.COMMIT_MESSAGE_NOT_FOUND);
+			throw new UserException(ExceptionCode.COMMIT_MESSAGE_NOT_FOUND);
 		}
 
 		return commits;
