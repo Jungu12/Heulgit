16d7bb3f40d02c258d607610a708469785a7f12e
안솔비 <caca30@naver.com>
2023-08-16 12:37:06 +0900

    fix(freeboard): 자유게시판 라우터 수정
    좋아요 유저 목록 페이지 라우터 수정

--- frontend/src/pages/freeboard/FreeBoardFeedItem.tsx
+++ frontend/src/pages/freeboard/FreeBoardFeedItem.tsx
@@ -172,7 +172,7 @@ const FreeBoardFeedItem = ({ feed, onClickComment }: Props) => {
 
 	// 좋아요 누른 유저 목록 페이지로 이동
 	const onClickLike = useCallback(() => {
-		navigation(`free/${feed.freeBoardId}/like`);
+		navigation(`${feed.freeBoardId}/like`);
 	}, [navigation]);
 
 	// 유레카 피드 상세보기 페이지로 이동
