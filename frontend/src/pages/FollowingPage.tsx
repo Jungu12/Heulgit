58be99844af920e89d32d48bb9485a26e17fbd03
백승은 <tmdms12@gmail.com>
2023-08-16 14:58:51 +0900

    test(mypage): 팔로잉 목록 api 요청 테스트

--- frontend/src/pages/FollowingPage.tsx
+++ frontend/src/pages/FollowingPage.tsx
@@ -28,14 +28,14 @@ const StyledUserList = styled.div`
 `;
 
 const FollowingPage = () => {
-	const { loadedUserId } = useParams();
-	console.log(loadedUserId);
+	const { userId } = useParams();
+
 	// 팔로잉 목록
 	useEffect(() => {
 		authHttp
-			.get<UserFollowingType[]>(`relations/followings/${loadedUserId}`)
+			.get<UserFollowingType[]>(`relations/followings/${userId}`)
 			.then((response) => {
-				console.log(loadedUserId);
+				console.log(userId);
 				console.log('팔로잉 로드 성공.', response);
 			})
 			.catch((error) => {
