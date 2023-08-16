c7731b06bc331aa5e79fb820ec7ead753982028d
백승은 <tmdms12@gmail.com>
2023-08-16 17:16:58 +0900

    feat(mypage): 팔로잉/팔로워 추가
    - 해당 유저 프로필 연결

--- frontend/src/pages/FollowerPage.tsx
+++ frontend/src/pages/FollowerPage.tsx
@@ -33,23 +33,23 @@ const FollowerPage = () => {
 		authHttp
 			.get<UserFollowingType[]>(`relations/followers/${userId}`)
 			.then((response) => {
-				console.log('팔로잉 로드 성공.', userId, response);
+				console.log('팔로워 로드 성공.', userId, response);
 				setUserFollowerData(response); // 받아온 데이터의 followers 배열을 상태에 저장
 			})
 			.catch((error) => {
-				console.error('팔로잉 로드 실패.', error);
+				console.error('팔로워 로드 실패.', error);
 			});
 	}, []);
 
 	return (
 		<StyledBox>
 			<StyledHeader>
-				<Header title={'팔로잉'}></Header>
+				<Header title={'팔로워'}></Header>
 			</StyledHeader>
 			<StyledUserList>
 				{userFollowerData &&
-					userFollowerData.map((user) => (
-						<FollowUser key={user.id} userData={user} />
+					userFollowerData.map((followUser) => (
+						<FollowUser userData={followUser} />
 					))}
 			</StyledUserList>
 		</StyledBox>
