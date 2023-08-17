950018a46fe658b5b8cf65ee632c5bac3cc57ee3
백승은 <tmdms12@gmail.com>
2023-08-17 09:33:06 +0900

    feat(mypage): 내 게시물 연결
    - 프로필(유레카/자유) 내 게시물 연결
    - 내활동 좋아요 페이지들 수정
    - 프로필 카테고리 자유 활성화 아이콘 연결 수

--- frontend/src/pages/MyActivity/LikeFreeboard.tsx
+++ frontend/src/pages/MyActivity/LikeFreeboard.tsx
@@ -1,30 +1,18 @@
 import FreeBoardFeedItem from '@pages/freeboard/FreeBoardFeedItem';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { FreeBoarFeedResponseType } from '@typedef/community/freeboard.types';
-import { UserLikePostType } from '@typedef/profile/user.types';
 import { authHttp } from '@utils/http';
-import React, { useEffect } from 'react';
+import React from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 
 const LikeFreeboard = () => {
 	// 좋아요 게시물 불러오기
-	useEffect(() => {
-		authHttp
-			.get<UserLikePostType[]>('users/activities/eureka/my-likes?pages=1')
-			.then((response) => {
-				console.log('좋아요 자유 성공.', response);
-			})
-			.catch((error) => {
-				console.error('좋아요 자유 실패.', error);
-			});
-	}, []);
-
 	const {
-		data: freeBoardLikeList,
-		fetchNextPage: freeBoardFetchNextPage,
-		hasNextPage: freeBoardHasNextPage,
+		data: freeLikeList,
+		fetchNextPage: freeFetchNextPage,
+		hasNextPage: freeHasNextPage,
 	} = useInfiniteQuery(
-		['/my-likes/freeBoard'],
+		['/my-likes/heulgit'],
 		({ pageParam = 1 }) =>
 			authHttp.get<FreeBoarFeedResponseType>(
 				`users/activities/eureka/my-likes?pages=${pageParam}`,
@@ -39,19 +27,20 @@ const LikeFreeboard = () => {
 
 	return (
 		<div>
-			{freeBoardLikeList && (
+			{freeLikeList && (
 				<InfiniteScroll
-					dataLength={freeBoardLikeList.pages.length}
-					next={freeBoardFetchNextPage}
-					hasMore={freeBoardHasNextPage ? true : false}
+					dataLength={freeLikeList.pages.length}
+					next={freeFetchNextPage}
+					hasMore={freeHasNextPage ? true : false}
 					loader={<div>loading...</div>}
+					height={`calc(100vh - 102px)`}
 					style={{
 						overflowY: 'scroll',
 						overflowX: 'hidden',
 					}}
 				>
-					{freeBoardLikeList.pages.map((freeBoard) =>
-						freeBoard.content.map((item) => (
+					{freeLikeList.pages.map((free) =>
+						free.content.map((item) => (
 							<FreeBoardFeedItem key={item.freeBoardId} feed={item} />
 						)),
 					)}
