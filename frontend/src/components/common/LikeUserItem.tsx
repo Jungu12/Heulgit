4924269584c20017dcd01e2aecdff3781644afc6
ksg2388 <ksg2388@naver.com>
2023-08-17 17:47:10 +0900

    fix(all): 좋아요 목록 페이지에서 팔로우 가능하도록 수정

---
+++ frontend/src/components/common/LikeUserItem.tsx
@@ -0,0 +1,114 @@
+import { colors } from '@constants/colors';
+import { UserType } from '@typedef/common.types';
+import { authHttp } from '@utils/http';
+import React, { useCallback, useState } from 'react';
+import { styled } from 'styled-components';
+
+type StyledFollowButtonProps = {
+	$following?: boolean;
+};
+
+const StyledLikeUser = styled.div`
+	display: flex;
+	position: relative;
+	justify-content: space-between;
+
+	height: 72px;
+
+	margin-bottom: 32px;
+`;
+
+// 좋아요 한 사람 프로필 컨테이너
+const StyledUserProfileContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+
+	margin-left: 20px;
+`;
+
+// 좋아요 한 사람 프로필 이미지
+const StyledProfileImg = styled.img`
+	display: flex;
+	width: 72px;
+	height: 72px;
+	border-radius: 50%;
+`;
+
+// 좋아요 한 사람 닉네임
+const StyledUserName = styled.div`
+	display: flex;
+	align-items: center;
+
+	margin-left: 20px;
+
+	font-size: 16px;
+	font-weight: 600;
+`;
+
+// 팔로우 팔로잉 버튼 컨테이너
+const StyledFollowButtonContainer = styled.div`
+	display: flex;
+	position: relative;
+	align-items: center;
+
+	margin-right: 20px;
+`;
+
+// 팔로우 팔로잉 버튼
+const StyledFollowButton = styled.button<StyledFollowButtonProps>`
+	align-items: center;
+
+	width: 93px;
+	height: 33px;
+
+	border-radius: 8px;
+
+	font-size: 14px;
+	font-weight: 600;
+
+	background-color: ${(props) =>
+		props.$following ? colors.greyScale.grey3 : colors.primary.primary};
+	color: ${(props) => (props.$following ? 'black' : 'white')};
+`;
+
+type Props = {
+	user: {
+		user: UserType;
+		follow: boolean;
+	};
+};
+
+const LikeUserItem = ({ user }: Props) => {
+	const [isFollowed, setIsFollowed] = useState(user.follow);
+
+	const onClickFollowButton = useCallback(() => {
+		if (isFollowed) {
+			authHttp
+				.delete(`relations/unfollow?to=${user.user.githubId}`)
+				.then(() => setIsFollowed(false));
+		} else {
+			authHttp
+				.post(`relations/follow?to=${user.user.githubId}`)
+				.then(() => setIsFollowed(true));
+		}
+	}, []);
+
+	return (
+		<StyledLikeUser key={user.user.githubId}>
+			<StyledUserProfileContainer>
+				<StyledProfileImg src={user.user.avatarUrl} alt="Profile" />
+				<StyledUserName>{user.user.githubId}</StyledUserName>
+			</StyledUserProfileContainer>
+			<StyledFollowButtonContainer>
+				<StyledFollowButton
+					$following={user.follow}
+					onClick={onClickFollowButton}
+				>
+					{isFollowed ? '팔로잉' : '팔로우'}
+				</StyledFollowButton>
+			</StyledFollowButtonContainer>
+		</StyledLikeUser>
+	);
+};
+
+export default LikeUserItem;
--- frontend/src/pages/LikeViewPage.tsx
+++ frontend/src/pages/LikeViewPage.tsx
@@ -9,6 +9,7 @@ import { findParams } from '@utils/relation';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import Loading from '@components/common/Loading';
+import LikeUserItem from '@components/common/LikeUserItem';
 
 // 좋아요 한 사람 페이지 전체 컨테이너
 const StyledLikeViewPageContainer = styled.div`
@@ -66,77 +67,6 @@ const StyledUserContainer = styled.div`
 	height: 100%;
 
 	/* bottom: 70px; */
-`;
-
-// 좋아요 한 사람 프로필 + 버튼 감싸는 컨테이너
-const StyledLikeUser = styled.div`
-	display: flex;
-	position: relative;
-	justify-content: space-between;
-
-	height: 72px;
-
-	margin-bottom: 32px;
-`;
-
-// 좋아요 한 사람 프로필 컨테이너
-const StyledUserProfileContainer = styled.div`
-	display: flex;
-	flex-direction: row;
-
-	margin-left: 20px;
-`;
-
-// 좋아요 한 사람 프로필 이미지
-const StyledProfileImg = styled.img`
-	display: flex;
-
-	width: 72px;
-	height: 72px;
-
-	border-radius: 50%;
-	background-color: #000000;
-`;
-
-// 좋아요 한 사람 닉네임
-const StyledUserName = styled.div`
-	display: flex;
-	align-items: center;
-
-	margin-left: 20px;
-
-	font-size: 16px;
-	font-weight: 600;
-`;
-
-// 팔로우 팔로잉 버튼 컨테이너
-const StyledFollowButtonContainer = styled.div`
-	display: flex;
-	position: relative;
-	align-items: center;
-
-	margin-right: 20px;
-`;
-
-type StyledFollowButtonProps = {
-	$following?: boolean;
-};
-
-// 팔로우 팔로잉 버튼
-const StyledFollowButton = styled.button<StyledFollowButtonProps>`
-	align-items: center;
-
-	width: 93px;
-	height: 33px;
-
-	border-radius: 8px;
-
-	font-size: 14px;
-	font-weight: 600;
-
-	background-color: ${(props) =>
-		props.$following ? colors.greyScale.grey3 : colors.primary.primary};
-	color: ${(props) => (props.$following ? 'black' : 'white')};
 `;
 
 const LikeViewPage: React.FC = () => {
@@ -197,19 +127,7 @@ const LikeViewPage: React.FC = () => {
 						}}
 					>
 						{likeList?.pages.map((users) =>
-							users.content.map((user) => (
-								<StyledLikeUser key={user.user.githubId}>
-									<StyledUserProfileContainer>
-										<StyledProfileImg src={user.user.avatarUrl} alt="Profile" />
-										<StyledUserName>{user.user.githubId}</StyledUserName>
-									</StyledUserProfileContainer>
-									<StyledFollowButtonContainer>
-										<StyledFollowButton $following={user.follow}>
-											{user.follow ? '팔로잉' : '팔로우'}
-										</StyledFollowButton>
-									</StyledFollowButtonContainer>
-								</StyledLikeUser>
-							)),
+							users.content.map((user) => <LikeUserItem user={user} />),
 						)}
 					</InfiniteScroll>
 				) : (
